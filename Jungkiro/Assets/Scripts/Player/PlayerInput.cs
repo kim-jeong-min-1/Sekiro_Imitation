e60066dba14722d102de64ee62282411cfe3810b
JeongminKim <kjmgi0671@naver.com>
2022-11-27 14:43:27 +0900

    change

--- Jungkiro/Assets/Scripts/Player/PlayerInput.cs
+++ Jungkiro/Assets/Scripts/Player/PlayerInput.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
